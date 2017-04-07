77e0244fb7c125ec874e873f87b660ab4a2fa349
Julia Yi <juliamijiyi@gmail.com>
2017-04-06 20:35:06 -0700

    Updated get winner function

--- js/winlogic.js
+++ js/winlogic.js
@@ -1,7 +1,33 @@
-function getWinner() {
+function getWinner(grid) {
+  var allCombos = [];
 
-  // createRows(grid);
-  // getFourConsec(grid);
+  // push all rows into allCombos
+  // TODO: fix me
+  // console.log(createRows(grid))
+
+  // push all diagonals into allCombos
+  var allDiagonals = generateAllDiagonals(grid)
+  for (var i = 0; i < allDiagonals.length; i++) {
+    allCombos.push(allDiagonals[i])
+  }
+
+  // push all columns into allCombos
+  for (var i = 0; i < grid.length; i++) {
+    allCombos.push(grid[i]);
+  }
+  console.log('allCombos=', JSON.stringify(allCombos))
+
+  //use get four consec function to look inside each combo of all combos
+
+  // find four consecutive cells
+
+  // add value of those cells
+
+  // check if 4 or -4
+
+  // determine winner
+
+  // if no winner, tie game
 
 }
 
@@ -32,7 +58,7 @@ function test() {
     [0,        0,       0,       0,       0,         0]
   ];
   renderBoard(grid);
-  console.log(stepUp([6, 5], grid));
+  getWinner(grid)
 }
 
 function stepUp(starting_point, grid) {
@@ -41,30 +67,12 @@ function stepUp(starting_point, grid) {
   var j = starting_point[1];
   var i_length = grid.length - 1;
   var j_length = grid[0].length - 1;
-  // while i & j are positive, decrement i & j by one
   while (i >= 0 && j >= 0) {
-    console.log(i, j);
-    console.log(i, j);
-    console.log(grid[i][j]);
-    console.log('d=',diagonal);
     diagonal.push(grid[i][j]);
     i-=1;
     j-=1;
   }
-  console.log(diagonal);
-
   return diagonal;
-}
-
-function generateAllDiagonals(grid) {
-  var allDiagonals = [];
-  grid[0].forEach(function(element, index) {
-    allDiagonals.push(stepDown([0, index], grid));
-  })
-  // grid.forEach(function(element, index) {
-  //   allDiagonals.push(stepDown([index, 0], grid));
-  // })
-  console.log(allDiagonals);
 }
 
 function stepDown(starting_point, grid) {
@@ -82,6 +90,35 @@ function stepDown(starting_point, grid) {
   return diagonal;
 }
 
+function generateAllDiagonals(grid) {
+  var allDiagonals = [];
+  allDiagonals.push(stepDown([0, 5], grid));
+  allDiagonals.push(stepDown([0, 4], grid));
+  allDiagonals.push(stepDown([0, 3], grid));
+  allDiagonals.push(stepDown([0, 2], grid));
+  allDiagonals.push(stepDown([0, 1], grid));
+  allDiagonals.push(stepDown([0, 0], grid));
+  allDiagonals.push(stepDown([1, 5], grid));
+  allDiagonals.push(stepDown([2, 5], grid));
+  allDiagonals.push(stepDown([3, 5], grid));
+  allDiagonals.push(stepDown([4, 5], grid));
+  allDiagonals.push(stepDown([5, 5], grid));
+  allDiagonals.push(stepDown([6, 5], grid));
+  allDiagonals.push(stepUp([6, 5], grid));
+  allDiagonals.push(stepUp([6, 4], grid));
+  allDiagonals.push(stepUp([6, 3], grid));
+  allDiagonals.push(stepUp([6, 2], grid));
+  allDiagonals.push(stepUp([6, 1], grid));
+  allDiagonals.push(stepUp([6, 0], grid));
+  allDiagonals.push(stepUp([5, 5], grid));
+  allDiagonals.push(stepUp([4, 5], grid));
+  allDiagonals.push(stepUp([3, 5], grid));
+  allDiagonals.push(stepUp([2, 5], grid));
+  allDiagonals.push(stepUp([1, 5], grid));
+  allDiagonals.push(stepUp([0, 5], grid));
+  return allDiagonals
+
+}
 
 function getFourConsec(array) { //grabs four consecutive spaces within each array
   var comparisons = [];
